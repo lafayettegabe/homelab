d78573c46e66a3bb256e23b94ff2e95336f89ca5
lafayettegabe <gabriel.soares@appmax.com.br>
2025-09-30 23:31:38 -0300

    Remove firewall config from host file to avoid conflicts

--- hosts/server1.nix
+++ hosts/server1.nix
@@ -3,7 +3,5 @@
   networking.hostName = "homelab";
   networking.useDHCP = lib.mkDefault true;
 
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 6443 10250 ];
-  networking.firewall.allowedUDPPorts = [ 8472 ];
+  # Firewall configuration moved to base.nix
 }
